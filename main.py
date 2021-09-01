0b4b41215c228a14e20fbb27b0db7a6a71a0d92f
pjroeder <pjroeder@iu.edu>
2021-08-31 21:14:25 -0400

    updated

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
 
 passage_name = "West of House"
 passage_text = "This is an open field west of a white house, with a boarded front door."
+
+print("You are at the " + passage_name)
+print(passage_text)
+response = input("What would you like to do? ")
+if response == "go north":
+    print("Good Job!")
